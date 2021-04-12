03f76e82d6bae606b7e264e0de66d376a65b95be
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-04-12 09:01:48 -0300

    feat: add components

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useChallenges } from '../hooks/useChallenges';
+
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  const { level } = useChallenges();
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/melquisedecfelipe.png" alt="Melquisedec Felipe "/>
+      <div>
+        <strong>Melquisedec Felipe</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
